c2cc5db18e5881bffa267d18a75c8d34e9e1f6fa
vsegda_vinovat <riabko97@gmail.com>
2024-02-23 18:31:19 +0200

    added article schemas

--- schemas.py
+++ schemas.py
@@ -1,6 +1,52 @@
-from pydantic import BaseModel
+from typing import List
+from pydantic import BaseModel, ConfigDict
+
+
+
+class Article(BaseModel):
+    title:str
+    content:str
+    published:bool
+    
+    model_config = ConfigDict(from_attributes=True)
+    
+
+class ArticleBase(BaseModel):
+    title:str
+    content:str
+    published:bool
+    creator_id:int
+    
+    
+    
+    
+class User(BaseModel):
+    id:int
+    username:str
+    
+        
+
+
+class ArticleDisplay(BaseModel):
+    title:str
+    content:str
+    published:bool
+    user: User
+    
+    model_config = ConfigDict(from_attributes=True)
+    
+
 
 class UserBase(BaseModel):
     username:str
     email:str
     password:str
+    
+    
+class UserDisplay(BaseModel):
+    username: str
+    email:str
+    items: List[Article] = []
+    
+    
+    model_config = ConfigDict(from_attributes=True)
