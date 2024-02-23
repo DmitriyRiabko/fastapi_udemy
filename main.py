ebb38a4267aca8796a4b90f6d9c33ced78040ecf
vsegda_vinovat <riabko97@gmail.com>
2024-02-23 18:31:48 +0200

    connection new router

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI, status, Response
 from contextlib import asynccontextmanager
+
+from router import user
 from router import blog_get
 from router import blog_post
+from router import article
+from db import models
+from db.database import engine
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # start
+    models.Base.metadata.create_all(engine)
     yield
     # end
 
@@ -13,12 +19,9 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="practise", lifespan=lifespan)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+app.include_router(user.router)
+app.include_router(article.router)
 
-
-
-@app.get("/hello")
-async def root():
-    return {"message": "Hello Udemy"}
 
 
 
