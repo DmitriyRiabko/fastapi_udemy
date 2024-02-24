0a2cdfdfe24fbd6e4437f7eeb41b3d7553ad8fa8
vsegda_vinovat <riabko97@gmail.com>
2024-02-24 12:52:32 +0200

    _

--- router/user.py
+++ router/user.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from schemas import UserBase, UserDisplay
 from db.database import get_db
 from db import db_user
+from auth.oauth2 import get_current_user
 
 
 router = APIRouter(prefix="/user", tags=["user"])
@@ -19,20 +20,35 @@ def create_user(request: UserBase, response: Response, db: Session = Depends(get
 
 
 @router.get("/", response_model=List[UserDisplay])
-def get_all_users(db: Session = Depends(get_db)):
+def get_all_users(
+    db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)
+):
     return db_user.get_all_users(db)
 
 
 @router.get("/{user_id}", response_model=UserDisplay)
-def get_user(user_id: int, db: Session = Depends(get_db)):
+def get_user(
+    user_id: int,
+    db: Session = Depends(get_db),
+    current_user: UserBase = Depends(get_current_user),
+):
     return db_user.get_user(db, user_id)
 
 
 @router.patch("/{id}/update")
-def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+def update_user(
+    id: int,
+    request: UserBase,
+    db: Session = Depends(get_db),
+    current_user: UserBase = Depends(get_current_user),
+):
     return db_user.update_user(db, id, request)
 
 
-@router.delete('/{id}/delete')
-def delete_user(id:int, db:Session = Depends(get_db)):
+@router.delete("/{id}/delete")
+def delete_user(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: UserBase = Depends(get_current_user),
+):
     return db_user.delete_user(db, id)
