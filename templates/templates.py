ecb60da29823d575691ce1e60fe0838cbdd64f34
vsegda_vinovat <riabko97@gmail.com>
2024-02-24 16:36:09 +0200

    added templates object

---
+++ templates/templates.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+router = APIRouter(prefix="/templates", tags=["templates"])
+templates = Jinja2Templates(directory="templates")
+
+
+
+@router.get('/products/{id}',response_class=HTMLResponse)
+def get_product(id:str,request:Request):
+    return templates.TemplateResponse(
+        'product.html',
+        {
+            'request':request,
+            'id':id
+        }
+        
+    )
+    
