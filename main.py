48695218ecc371a820a0225405382e7f028b4c49
vsegda_vinovat <riabko97@gmail.com>
2024-02-24 12:50:31 +0200

    added CORS middleware

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, status, Response, Request
 from contextlib import asynccontextmanager
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from router import user
 from router import blog_get
 from router import blog_post
 from router import article
+from router import product
+from auth import authentication
+
 from db import models
 from db.database import engine
+from exceptions import StoryException
 
 
 @asynccontextmanager
@@ -17,11 +23,25 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="practise", lifespan=lifespan)
+app.include_router(authentication.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 app.include_router(user.router)
 app.include_router(article.router)
+app.include_router(product.router)
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
+app.exception_handler(StoryException)
+
+
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(status_code=418, content={"detail": exc.name})
